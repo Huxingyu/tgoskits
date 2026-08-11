907922205f5487933da52eed2acd69d5e1a7844a
Joe Hu <elgooguknow@gmail.com>
2026-08-11 18:01:36 +0800

    feat(task3-ai): pass TASK3_CONTROL_LOOP through Linux build script

--- scripts/test/net-dual-guest/build-linux-task2.sh
+++ scripts/test/net-dual-guest/build-linux-task2.sh
@@ -35,6 +35,7 @@ build_role() {
         TASK2_ROLE="$role" \
         TASK2_LOCAL_IP="$local_ip" \
         TASK2_PEER_IP="$peer_ip" \
+        ${TASK3_CONTROL_LOOP:+TASK3_CONTROL_LOOP="$TASK3_CONTROL_LOOP"} \
         CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="$cross_cc" \
         cargo build \
         --package arceos-task2-net \
